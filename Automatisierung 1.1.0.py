054a24062edf1f162e8cb831488a034f4ee051b9
Isaac <humphries.laughter@gmail.com>
2021-02-21 08:27:00 -0700

    Added utility functions for setting changes

--- Automatisierung 1.1.0.py
+++ Automatisierung 1.1.0.py
@@ -1,9 +1,7 @@
-from logging import PlaceHolder
-import os, discord
-from discord.ext import commands
-from discord.flags import MessageFlags
+import discord
+from discord.ext import commands, tasks
 from discord.utils import get
-from discord.ext import tasks
+
 TOKEN = "Nzg5ODc2NDg2Njk3NjQ4MTY5.X94bzQ.mjYt_9zm3fa5CQ7ZzG-L0NE_vu8"
 GUILD = "Bot test server"
 
@@ -17,42 +15,40 @@ Results = []
 
 @bot.event
 async def on_ready():
-    global Guild
+    global Guild, ThroneRoom, Announcements
     Guild = get(bot.guilds, name = GUILD)
     ThroneRoom = Guild.get_channel(794271850964189204)
     Announcements = Guild.get_channel(794668206119190539)
     #Confirm its ready:
     print("Up and running")
 
-    @tasks.loop(seconds = 10)
-    async def ThroneRoomLoop():
-        global ThroneRoomActive
-        if (ThroneRoomLoop.current_loop % 2) == 0:
-            Active = False
-        else:
-            Active = True
+@tasks.loop(seconds = 10)
+async def ThroneRoomLoop():
+    global ThroneRoomActive
+    if (ThroneRoomLoop.current_loop % 2) == 0:
+        Active = False
+    else:
+        Active = True
 
-        for Role in Guild.roles:
-            if Role.name == "TEst Admin":
-                Citizenry = Role
-                
-        if Active:
-            for message in Messages:
-                await message["Message"].delete()
-
-            await ThroneRoom.set_permissions(Citizenry, view_channel = True)
-            await Announcements.send("#throne-room is now open all yall!")
-        else:
-            await ThroneRoom.set_permissions(Citizenry, view_channel = False)
-            await Announcements.send("#throne-room has closed. Voting will be held and results will be announced soon.")
+    for Role in Guild.roles:
+        if Role.name == "TEst Admin":
+            Citizenry = Role
             
-            for message in Messages:
-                await message["Message"].add_reaction("👍")
-                await message["Message"].add_reaction("👎")
+    if Active:
+        for message in Messages:
+            await message["Message"].delete()
 
-        ThroneRoomActive = Active
+        await ThroneRoom.set_permissions(Citizenry, view_channel = True)
+        await Announcements.send("#throne-room is now open all yall!")
+    else:
+        await ThroneRoom.set_permissions(Citizenry, view_channel = False)
+        await Announcements.send("#throne-room has closed. Voting will be held and results will be announced soon.")
+        
+        for message in Messages:
+            await message["Message"].add_reaction("👍")
+            await message["Message"].add_reaction("👎")
 
-    ThroneRoomLoop.start()
+    ThroneRoomActive = Active
 
 @bot.command()
 async def RequestRank(ctx):
@@ -107,16 +103,52 @@ async def on_raw_reaction_add(payload):
     ThroneRoom = Guild.get_channel(794271850964189204)
     Placement = 0
     #ReactionCount = get(payload.message.reactions, emoji=payload.emoji.name).count
-
     if (payload.channel_id == ThroneRoom.id) and not ThroneRoomActive:
         for message in Messages:
             if message["Message"] == payload.message and Messages[Placement]["Role"].name == "Regent":
                 if payload.member.top_role.name != "Regent":
                     await message["Message"].remove_reaction(payload.emoji,payload.member)
 
+@bot.command()
+async def LoopSettings():
+    async def Restart(ctx):
+        Member = ctx.message.member
+        if Member.top_role.name != "Regent":
+            await ctx.send("You do not have permission to access this command.")
+        else:
+            ThroneRoomLoop()
+            await ctx.send("The #throne-room open/close cycle has been restarted. The current loop time is: " + str(ThroneRoomLoop.next_iteration))
+            await ThroneRoomLoop.restart()
+
+    async def Start(ctx):
+        Member = ctx.message.member
+        if Member.top_role.name != "Regent":
+            await ctx.send("You do not have permission to access this command.")
+        else:
+            if ThroneRoomLoop.running():
+                if ThroneRoomActive:
+                    await ctx.send("The #throne-room loop is already running. #throne-room is already open and will close on: " + str(ThroneRoomLoop.next_iteration))
+                else:
+                    await ctx.send("The #throne-room loop has been started")
+                    await ThroneRoomLoop.start()
+    
+    async def Stop(ctx):
+        Member = ctx.message.member
+        if Member.top_role.name != "Regent":
+            await ctx.send("You do not have permission to access this command.")
+        else:
+            if not ThroneRoomLoop.running():
+                await ctx.send("The #throne-room loop is not running.")
+            else:
+                await ctx.send("The #throne-room loop has been stopped.")
+                await ThroneRoomLoop.cancel()
+
+
 @bot.listen("on_message")
 async def Responder(message):
     if message.content == "E":
         await message.channel.send("Oh?")
+
+
 
 bot.run(TOKEN)  
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Automatisierung
 Discord Bot For Automating Some Things In a Server
+
+Commands available:
+	Prefix - TNSS
+	Command 1 - RequestRank
+		Will check the message authors roles and post a message saying the user wwould like to advance to the lext level of ranks after deleting the message posted by the user.
